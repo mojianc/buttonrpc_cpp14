4c5170bb67e2fa34c3a78b41b59cea8159d56310
mojianc <498268626@qq.com>
2020-06-20 19:02:27 +0800

    创建demo，测试zeroMQ，参考网址：https://blog.csdn.net/jiang1013nan/article/details/19671533?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.nonecase

---
+++ build-vs2015/customrpc-demo/customrpc-demo-client/customrpc-demo-client.cpp
@@ -0,0 +1,30 @@
+// customrpc-demo-client.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <zmq.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	//  Socket to talk to clients
+	void *context = zmq_ctx_new();
+	void *responder = zmq_socket(context, ZMQ_REP);
+	int rc = zmq_bind(responder, "tcp://*:5555");
+	assert(rc == 0);
+
+	while (1) {
+		char buffer[10];
+		zmq_recv(responder, buffer, 10, 0);
+		printf("Received Hello\n");
+		Sleep(1);          //  Do some 'work'
+		zmq_send(responder, "World", 5, 0);
+	}
+	return 0;
+}
+
